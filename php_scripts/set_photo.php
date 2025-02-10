c773893d9e398f4ad3857a9db4de4287bb867eab
Fakejob1337 <savahova@gmail.com>
2025-02-10 10:06:50 +0700

    fix_ photo

--- php_scripts/set_photo.php
+++ php_scripts/set_photo.php
@@ -8,7 +8,7 @@ if ($_FILES && $_FILES["photo"]["error"]== UPLOAD_ERR_OK)
     $status = move_uploaded_file($_FILES["photo"]["tmp_name"], $name);
     $conn = new PDO('mysql:host=localhost;dbname=ssc', 'root', 'p@$$word');
     $sql = "UPDATE `personal_information` SET `profile_photo`= '$db_name' WHERE shtat_id = '$id'";
-    $sql = "INSERT INTO `photos`(`shtat_id`, `file_path`) VALUES ('$id','$db_name')";
+    // $sql = "INSERT INTO `photos`(`shtat_id`, `file_path`) VALUES ('$id','$db_name')";
     $res = $conn->query($sql)->fetch();
 }
 
